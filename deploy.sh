9f0fca7db71e03c30709ce2d127123b039a064ca
Gustavo Henrique <gustavohenriquefs.dev@gmail.com>
2023-02-20 20:16:10 -0300

    fix pipelines: change route name

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ yarn install
 yarn build
 
 # Pega um arquivo(import-map) da nuvem(s3) e dá um alias, no caso: import-map
-aws s3 cp s3://mfe-shoppe/config/import-map.json import-map.json
+aws s3 cp s3://mfe-shoppe-aula/config/import-map.json import-map.json
 
 # Atualização da versão da URL
 NEW_URL=/config/mfe/app-header/$VERSION/shoppe-app-header.js
@@ -17,10 +17,10 @@ cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@shoppe/app-hea
 
 
 #UPLOAD build
-aws s3 cp dist s3://mfe-shoppe/config/mfe/app-header/$VERSION --recursive
+aws s3 cp dist s3://mfe-shoppe-aula/config/mfe/app-header/$VERSION --recursive
 
 # UPLOAD new import-map
-aws s3 cp new.importmap.json s3://mfe-shoppe/config/import-map.json
+aws s3 cp new.importmap.json s3://mfe-shoppe-aula/config/import-map.json
 
 # CACHE INVALIDATION
 aws cloudfront create-invalidation --distribution-id E2CSYO6LJ44WFR --paths '/config/import-map.json'
